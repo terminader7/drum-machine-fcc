c8c4c177d63c6a88ef752be96c80b0ca54feaeba
Nader Ebrahim~ <nebrahim2424@gmail.com>
2022-02-17 18:02:15 -0800

    set up button function

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -6,6 +6,14 @@ export const DrumMachine = () => {
     <div id="drum-machine">
       <div id="display">
         <DrumMachineButton id={"Q"} textValue={"Heater 1"} />
+        <DrumMachineButton id={"W"} textValue={"Heater 2"} />
+        <DrumMachineButton id={"E"} textValue={"Heater 3"} />
+        <DrumMachineButton id={"A"} textValue={"Heater 4"} />
+        <DrumMachineButton id={"S"} textValue={"Clap"} />
+        <DrumMachineButton id={"D"} textValue={"D"} />
+        <DrumMachineButton id={"Z"} textValue={"Kick n'Hat"} />
+        <DrumMachineButton id={"X"} textValue={"Kick"} />
+        <DrumMachineButton id={"C"} textValue={"Closed HH"} />
       </div>
     </div>
   );
